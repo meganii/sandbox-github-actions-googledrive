72569fa9c1324070bf5ecee65b785f47a20e482f
meganii <yuhei24@gmail.com>
2022-12-16 22:30:53 +0900

    Update

--- list.py
+++ list.py
@@ -3,4 +3,5 @@ from googleapiclient import discovery
 
 crds, prj = google.auth.default(scopes=["https://www.googleapis.com/auth/drive.metadata.readonly"])
 service = discovery.build("drive", "v3", credentials=crds)
-service.files().list().execute()
+result = service.files().list().execute()
+print(result)
