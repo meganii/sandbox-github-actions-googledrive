3b0cc2d76b65a200316b893bc27561b78b2d0401
meganii <yuhei24@gmail.com>
2022-12-16 22:23:26 +0900

    Add Scope

--- list.py
+++ list.py
@@ -1,6 +1,6 @@
 import google.auth
 from googleapiclient import discovery
 
-crds, prj = google.auth.default(scopes=["https://www.googleapis.com/auth/drive.file"])
+crds, prj = google.auth.default(scopes=["https://www.googleapis.com/auth/drive.metadata.readonly"])
 service = discovery.build("drive", "v3", credentials=crds)
 service.files().list().execute()
